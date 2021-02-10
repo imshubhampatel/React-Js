c1134e821ecd5f6871d4d06e039972982c9636d1
Shubham Patel <75959964+shubhampatel01@users.noreply.github.com>
2021-02-10 22:52:59 +0530

    WIP: First Day with React

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// var react = require('react'); we can also write this in react js 
+// var reactDom = require('react-dom');
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import React from "react";
+import ReactDom from "react-dom";
+import "./index.css";
+import { useState } from "react";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// this is javascript ES6 convenstion
+const fname = "Shubham";
+const lname = "Patel";
+const CurrentDate = new Date().toLocaleDateString();
+
+
+ReactDom.render(
+    <>
+    <h1>My name is {fname} {lname}</h1>
+    <h2>Now Date is {CurrentDate}</h2>
+    </>
+    ,document.getElementById('root'));
+
+
+  // reactDom.render(first parameter,second parameter
+  // first parameter = what to show on the react dom = this takes jsx
+  // second parameter = where to show the react dom = this takes root id
+
+// we passes the document.getElementById("root");
